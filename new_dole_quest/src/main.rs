e448cfe9ef322ce51be0a6db3d07d7aaf5b06556
Cody Thompson <47369629+TrueSgtMonkey@users.noreply.github.com>
2024-04-11 03:46:51 -0700

    creating ActionMap
    * For programmable actions instead of static keys
    * TODO: Need to add config file for keys instead of statically declaring
      them in code -- so that way we can save

--- new_dole_quest/src/main.rs
+++ new_dole_quest/src/main.rs
@@ -1,10 +1,17 @@
+use actions::ActionMap;
 use bevy::{prelude::*, window::WindowMode};
 use bevy::window::WindowResolution;
 
 pub mod components;
+pub mod actions;
+
+// TODO: Remove -- just for reference
+// #[derive(Resource)]
+// struct GreetTimer(Timer);
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins.set(WindowPlugin {
+    App::new().insert_resource(ActionMap::new())
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
         // TODO: Make most of these windows attributes non-static values
         primary_window: Some(Window {
             resolution: WindowResolution::new(800.0, 600.0),
@@ -27,3 +34,14 @@ fn main() {
         .add_plugins(components::ComponentsPlugin)
         .run();
 }
+
+// TODO: Remove -- just for reference
+// pub fn has_user_input(
+//     keyboard_input: Res<ButtonInput<KeyCode>>,
+//     mouse_button_input: Res<ButtonInput<MouseButton>>,
+//     touch_input: Res<Touches>,
+// ) -> bool {
+//     keyboard_input.just_pressed(KeyCode::Space)
+//         || mouse_button_input.just_pressed(MouseButton::Left)
+//         || touch_input.any_just_pressed()
+// }

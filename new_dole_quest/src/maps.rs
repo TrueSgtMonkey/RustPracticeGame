1fa42ea903d09fb30c407f9ade40a0409de4a5e1
Cody Thompson <47369629+TrueSgtMonkey@users.noreply.github.com>
2024-04-16 01:54:57 -0700

    Created level editor with Godot
    * Still need to make the tiles themselves more efficient

---
+++ new_dole_quest/src/maps.rs
@@ -0,0 +1,11 @@
+use bevy::prelude::*; // need this even in submodules
+
+pub mod map_importer;
+
+pub struct MapPlugin;
+
+impl Plugin for MapPlugin {
+    fn build(&self, app: &mut App) {
+        
+    }
+}

482c76fcc3a390e0278cd7a902f65832cb102697
Cody Thompson <47369629+TrueSgtMonkey@users.noreply.github.com>
2024-04-16 14:44:19 -0700

    Creating map materials and map builder
    * MapMaterial - needed for drawing rectangles with the coordinates that
      the map file specifies.
    
    * MapBuilder - needed to read map files and output visuals on the screen
      complete with collisions

--- new_dole_quest/src/maps.rs
+++ new_dole_quest/src/maps.rs
@@ -1,11 +1,14 @@
-use bevy::prelude::*; // need this even in submodules
+use bevy::prelude::*;
 
-pub mod map_importer;
+use self::map_builder::MapBuilderPlugin; // need this even in submodules
+
+pub mod map_builder;
+pub mod map_material;
 
 pub struct MapPlugin;
 
 impl Plugin for MapPlugin {
     fn build(&self, app: &mut App) {
-        
+        app.add_plugins(MapBuilderPlugin);
     }
 }
